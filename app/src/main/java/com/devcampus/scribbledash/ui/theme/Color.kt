8a6f21908ace596251f5a633128a643a2b5b4b3d
Andres Ruiz <andres-ruiz@sandhills.com>
2025-03-31 16:01:46 -0500

    setup theme

--- app/src/main/java/com/devcampus/scribbledash/ui/theme/Color.kt
+++ app/src/main/java/com/devcampus/scribbledash/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.devcampus.scribbledash.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ScribbleDashBlue = Color(0xFF238CFF)
+val ScribbleDashPurple = Color(0xFFAB5CFA)
+val ScribbleDashOrange = Color(0xFFFA852C)
+val ScribbleDashRed = Color(0xFFEF1242)
+val ScribbleDashGreen = Color(0xFF0DD280)
+val ScribbleDashWhite = Color(0xFFFFFFFF)
+val ScribbleDashWhite40 = Color(0x66FFFFFF)
+val ScribbleDashBackground = Color(0xFFFEFAF6)
+val ScribbleDashOnBackground = Color(0xFF514437)
+val ScribbleDashOnBackgroundVariant = Color(0xFF7F7163)
+val ScribbleDashSurfaceHigh = Color(0xFFFFFFFF)
+val ScribbleDashSurface = Color(0xCCFFFFFF)
+val ScribbleDashSurfaceLow = Color(0xFFEEE7E0)
+val ScribbleDashSurfaceLowest = Color(0xFFE1D5CA)
+val ScribbleDashOnSurface = Color(0xFFA5978A)
+val ScribbleDashOnSurfaceVariant = Color(0xFFF6F1EC)
--- app/src/main/java/com/devcampus/scribbledash/ui/theme/Type.kt
+++ app/src/main/java/com/devcampus/scribbledash/ui/theme/Type.kt
@@ -1,34 +1,128 @@
 package com.devcampus.scribbledash.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.devcampus.scribbledash.R
+
+val BagelFatOne = FontFamily(
+    Font(
+        resId = R.font.bagelfatone_regular,
+        weight = FontWeight.Normal
+    ),
+)
+
+val Outfit = FontFamily(
+    Font(
+        resId = R.font.outfit_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.outfit_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.outfit_semibold,
+        weight = FontWeight.SemiBold
+    )
+)
 
 // Set of Material typography styles to start with
+val Typography.headlineXSmall: TextStyle
+@Composable
+get() {
+    return TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+}
+
+val Typography.labelXLarge: TextStyle
+    @Composable
+    get() {
+        return TextStyle(
+            fontFamily = Outfit,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+            lineHeight = 28.sp
+        )
+    }
+
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 66.sp,
+        lineHeight = 80.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 40.sp,
+        lineHeight = 44.sp
+    ),
+
+
+    headlineLarge = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        lineHeight = 48.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 30.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 28.sp
+    ),
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Outfit,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        lineHeight = 24.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Outfit,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 28.sp
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = Outfit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
 )
